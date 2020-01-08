b51f16279e89d9fb33aee5a44ce3390c37b9c21a
Bernardo Ferreira <bernardo.caiano@outlook.pt>
2020-01-08 00:29:21 +0000

    continuação design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <navbar/>
+    <div class="margin">
+      <div class="has-padding-top-30 has-padding-bottom-50 center">
+        <h3 class="title is-4">Security Status</h3>
+      </div>
+      <h5 class="subtitle is-5">Report</h5>
+      <div class="box">
+        <p>ola</p>
+      </div>
+      <h5 class="subtitle is-5 has-padding-top-30">Offices</h5>
+      
+    </div>
   </div>
 </template>
 
+<style scoped>
+.margin {
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
+.center {
+  text-align: center;
+}
+
+.title {
+  color: white
+}
+
+.subtitle {
+  color: white
+}
+
+.box {
+  height: 200px;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import navbar from "@/components/navbar.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    navbar
   }
 };
 </script>
