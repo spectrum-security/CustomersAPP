eb9ff33bcc776c17eebeb2c6285d5b15d4727979
Bernardo Ferreira <bernardo.caiano@outlook.pt>
2020-01-16 00:11:22 +0000

    calendario funcional

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -5,7 +5,37 @@
       <div class="has-padding-top-30 has-padding-bottom-50 center">
         <h3 class="title is-4">Calendar</h3>
       </div>
-      <FunctionalCalendar></FunctionalCalendar>
+      <FunctionalCalendar
+        @choseDay="dateClick"
+        :is-multiple-date-picker="datePicker"
+        :key="datePicker"
+        :marked-dates="markedDates"
+        v-model="calendarData"
+      ></FunctionalCalendar>
+      <b-button
+        type="is-spectrum_blue"
+        v-if="schedule == true && confirm == false"
+        @click="pickDate"
+        >Schedule Maintenance
+      </b-button>
+      <b-button
+        type="is-spectrum_blue"
+        v-if="schedule == false && confirm == true"
+        @click="pickDate"
+        >Confirm
+      </b-button>
+      <!-- <b-button
+        type="is-spectrum_blue"
+        
+        >Unschedule Maintenance
+      </b-button> -->
+
+      <p v-for="(day, index) in datesPickedFinal" :key="index">
+        Maintenence schedule day {{ day.date }}
+      </p>
+      <b-button type="is-spectrum_blue" class="plusButton">
+        <i class="fas fa-plus"></i>
+      </b-button>
     </div>
   </div>
 </template>
@@ -27,6 +57,14 @@
 .subtitle {
   color: black;
 }
+
+.plusButton {
+  position: fixed;
+  right: 10px;
+  bottom: 74px;
+  z-index: 20;
+  border-radius: 80px;
+}
 </style>
 
 <script>
@@ -42,8 +80,45 @@ export default {
   },
   data() {
     return {
-      calendarData: {}
+      calendarData: {},
+      datePicker: false,
+      datesPicked: [],
+      datesPickedFinal: [],
+      markedDates: [],
+      schedule: true,
+      confirm: false,
+      days: [],
+      cont: 0,
+      flag: true
     };
+  },
+
+  created() {
+    //console.log(this.calendarData);
+  },
+
+  methods: {
+    pickDate() {
+      if (this.datePicker == false) this.datePicker = true;
+      else this.datePicker = false;
+
+      if (this.datePicker == false) {
+        this.datesPicked = this.calendarData.selectedDates;
+        for (let i = 0; i < this.datesPicked.length; i++) {
+          this.datesPickedFinal.push(this.datesPicked[i]);
+          this.markedDates.push(this.datesPicked[i].date);
+        }
+      }
+      this.confirm = false;
+      this.schedule = true;
+    },
+
+    dateClick() {
+      this.confirm = true;
+      this.schedule = false;
+    },
+
+    confirmFunc() {}
   }
 };
 </script>
